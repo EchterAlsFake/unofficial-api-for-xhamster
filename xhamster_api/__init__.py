ac62db843067866e6d02663be36ee3a511ba0c2b
Johannes Habel <EchterAlsFake@proton.me>
2026-07-26 21:23:03 +0200

    - switched to AGPL - release

--- xhamster_api/__init__.py
+++ xhamster_api/__init__.py
@@ -1,8 +1,8 @@
 import logging
 
-__all__ = ["Client", "Video", "consts"]
+__all__ = ["Client", "Video", "consts", "DownloadConfigHLS"]
 
-from xhamster_api.api import Client, Video
+from xhamster_api.api import Client, Video, DownloadConfigHLS
 from xhamster_api.modules import consts
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
